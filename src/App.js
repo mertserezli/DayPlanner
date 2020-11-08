293fff988e4f6803960a8bbaee12fee24931364a
Mert Serezli <mertserezli@gmail.com>
2020-11-08 18:44:34 +0300

    add project skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAR3BhiHFJEAgb-DjNF4UJqKyK3tg-TndI",
+    authDomain: "dayplanner-f78c2.firebaseapp.com",
+    databaseURL: "https://dayplanner-f78c2.firebaseio.com",
+    projectId: "dayplanner-f78c2",
+    storageBucket: "dayplanner-f78c2.appspot.com",
+    messagingSenderId: "626321664189",
+    appId: "1:626321664189:web:aed04b2f38bc2c7100efd4",
+    measurementId: "G-DF7GXSKEK8"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1 style={{textAlign:"center"}}>Day Planner</h1>
+
+        <section>
+          {user ? <DayPlanner /> : <SignIn />}
+        </section>
+      </div>
   );
 }
+
+function SignIn() {
+
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    };
+
+    return (
+        <div style={{textAlign:"center"}}>
+            <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
+    )
+
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+function DayPlanner() {
+    return(
+    <>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <main style={{grow: 1, display: "flex", flexDirection: "row"}}>
+                <aside style={{width: "20%"}}>
+                    {/*<Calendar/>*/}
+                </aside>
+                <article style={{flexGrow: "1"}}>
+                    {/*<TodoList/>*/}
+                </article>
+                <nav style={{width: "25%"}}>
+                    <SignOut/>
+                    {/*<AddTodo/>*/}
+                </nav>
+            </main>
+        </div>
+    </>
+    )
+}
+
+
 
 export default App;
