4aa50b711b605f1d54740bbe8feeb543505765a9
Mert Serezli <mertserezli@gmail.com>
2020-11-19 20:58:58 +0300

    add modal for adding elements

--- src/App.js
+++ src/App.js
@@ -86,6 +86,7 @@ function TodoList() {
         }): [], {key:"score", direction:"descending"});
 
     return(<>
+            <AddTodo/>
             <table>
                 <thead>
                 <tr>
@@ -102,7 +103,6 @@ function TodoList() {
                 )}
                 </tbody>
             </table>
-            <AddTodo/>
         </>
     )
 }
@@ -117,7 +117,7 @@ function TodoItem(props){
     const [value, setValue] = useState(item.value);
     const [time, setTime] = useState(item.time);
 
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
 
     const removeTodo = async (id)=>{
         query.doc(id).delete()
@@ -174,35 +174,53 @@ function AddTodo(){
     const [time, setTime] = useState('');
     const [description, setDescription] = useState('');
 
+    const [open, setOpen] = useState(false);
+
     const addTodo = async (e) => {
         e.preventDefault();
         const query = firestore.collection('Todo');
 
-        await query.add({name:title, value:parseInt(value), time:parseInt(time), description:description});
+        query.add({name:title, value:parseInt(value), time:parseInt(time), description:description});
 
         setTitle('');
         setValue('');
         setTime('');
         setDescription('');
+
+        setOpen(false)
+    };
+
+    const handleClose = ()=>{
+        setOpen(false)
     };
 
     return (
         <>
-            <form style={{grow: 1, display: "flex", flexDirection: "column", flexWrap: "wrap"}}>
-                <label htmlFor="title"><b>Title</b></label>
-                <input placeholder="Title" name="title" id="title"
-                       value={title} onChange={(e) => setTitle(e.target.value)}/>
-                <label htmlFor="value"><b>Value</b></label>
-                <input placeholder="INTRINSIC Value" type="number" name="value" id="value"
-                       value={value} onChange={(e) => setValue(e.target.value)}/>
-                <label htmlFor="time"><b>Time</b></label>
-                <input placeholder="Time(minutes)" type="number" name="time" id="time"
-                       value={time} onChange={(e) => setTime(e.target.value)}/>
-                <label htmlFor="description"><b>Description</b></label>
-                <textarea placeholder="Description" name="description" id="description"
-                          value={description} onChange={(e) => setDescription(e.target.value)}/>
-                <button type="submit" onClick={addTodo}>Add</button>
-            </form>
+            <button onClick={()=>setOpen(true)}>Add</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div style={{display: "flex", flexDirection: "column", top:"50%", left:"50%", width:"40%", transform:"translate(70%, 80%)", backgroundColor:"white", padding: "25px", borderRadius: "10px"}}>
+                    <form style={{grow: 1, display: "flex", flexDirection: "column", flexWrap: "wrap"}}>
+                        <label htmlFor="title"><b>Title</b></label>
+                        <input placeholder="Title" name="title" id="title"
+                               value={title} onChange={(e) => setTitle(e.target.value)}/>
+                        <label htmlFor="value"><b>Value</b></label>
+                        <input placeholder="INTRINSIC Value" type="number" name="value" id="value"
+                               value={value} onChange={(e) => setValue(e.target.value)}/>
+                        <label htmlFor="time"><b>Time</b></label>
+                        <input placeholder="Time(minutes)" type="number" name="time" id="time"
+                               value={time} onChange={(e) => setTime(e.target.value)}/>
+                        <label htmlFor="description"><b>Description</b></label>
+                        <textarea placeholder="Description" name="description" id="description"
+                                  value={description} onChange={(e) => setDescription(e.target.value)}/>
+                        <button type="submit" onClick={addTodo}>Add</button>
+                    </form>
+                </div>
+            </Modal>
         </>
     )
 }
@@ -231,6 +249,7 @@ function PeriodicTodoList() {
     const [TodoItems] = useCollectionData(query,{ idField: 'id' });
 
     return(<>
+            <AddPeriodicTodo/>
             <table>
                 <thead>
                 <tr>
@@ -245,7 +264,6 @@ function PeriodicTodoList() {
                 )}
                 </tbody>
             </table>
-            <AddPeriodicTodo/>
         </>
     )
 }
@@ -259,7 +277,7 @@ function PeriodicItem(props){
     const [title, setTitle] = useState(item.name);
     const [period, setPeriod] = useState(item.period);
 
-    const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = useState(false);
 
     const removePeriodicTodo = async (id)=>{
         query.doc(id).delete()
@@ -311,6 +329,8 @@ function AddPeriodicTodo(){
     const [period, setPeriod] = useState('');
     const [description, setDescription] = useState('');
 
+    const [open, setOpen] = useState(false);
+
     const addTodo = async (e) => {
         e.preventDefault();
         const query = firestore.collection('PeriodicTodo');
@@ -322,8 +342,20 @@ function AddPeriodicTodo(){
         setDescription('');
     };
 
+    const handleClose = ()=>{
+        setOpen(false)
+    };
+
     return (
         <>
+            <button onClick={()=>setOpen(true)}>Add Periodic</button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+            >
+                <div style={{display: "flex", flexDirection: "column", top:"50%", left:"50%", width:"40%", transform:"translate(70%, 80%)", backgroundColor:"white", padding: "25px", borderRadius: "10px"}}>
             <form style={{grow: 1, display: "flex", flexDirection: "column", flexWrap: "wrap"}}>
                 <label htmlFor="title"><b>Title</b></label>
                 <input placeholder="Title" name="title" id="title"
@@ -336,6 +368,8 @@ function AddPeriodicTodo(){
                           value={description} onChange={(e) => setDescription(e.target.value)}/>
                 <button type="submit" onClick={addTodo}>Add</button>
             </form>
+                </div>
+            </Modal>
         </>
     )
 }
