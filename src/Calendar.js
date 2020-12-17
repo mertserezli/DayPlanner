ddbb0032d5e734e40539b9b5cc381f1cb10d73b5
Mert Serezli <mertserezli@gmail.com>
2020-12-17 15:13:56 +0300

    add import export for calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -43,7 +43,8 @@ export default class Demo extends React.PureComponent {
             data:[],
         };
 
-        this.onCommitChanges = this.commitChanges.bind(this);
+        this.commitChanges = this.commitChanges.bind(this);
+        this.importJSON = this.importJSON.bind(this);
     }
 
     componentDidMount() {
@@ -84,24 +85,81 @@ export default class Demo extends React.PureComponent {
         this.query.set({data});
     }
 
+    exportToJson(object){
+        let filename = "export.json";
+        let contentType = "application/json;charset=utf-8;";
+        object = object.map(o => {
+            o.startDate = o.startDate.toString();
+            o.endDate = o.endDate.toString();
+            return o
+        });
+        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            let blob = new Blob([decodeURIComponent(encodeURI(JSON.stringify(object)))], { type: contentType });
+            navigator.msSaveOrOpenBlob(blob, filename);
+        } else {
+            let a = document.createElement('a');
+            a.download = filename;
+            a.href = 'data:' + contentType + ',' + encodeURIComponent(JSON.stringify(object));
+            a.target = '_blank';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    }
+
+    importJSON(e){
+        const fileReader = new FileReader();
+        fileReader.readAsText(e.target.files[0], "UTF-8");
+        let curId = Math.max(...this.state.data.map(d => d.id)) + 1;
+        fileReader.onload = e => {
+            let appointmentsToImport = JSON.parse(e.target.result);
+            appointmentsToImport.forEach(a => {
+                a.startDate = new Date(a.startDate);
+                a.endDate = new Date(a.endDate);
+
+                const curStart = new Date();
+                curStart.setHours(a.startDate.getHours());
+                curStart.setMinutes(a.startDate.getMinutes());
+                a.startDate = curStart;
+
+                const curEnd = new Date();
+                curEnd.setHours(a.endDate.getHours());
+                curEnd.setMinutes(a.endDate.getMinutes());
+                a.endDate = curEnd;
+
+                a.id = curId;
+                curId++;
+            });
+            appointmentsToImport = [...appointmentsToImport, ...this.state.data];
+            this.query.set({data:appointmentsToImport});
+        };
+    };
+
     render() {
         const { data } = this.state;
 
         return (
-            <Paper>
-                <Scheduler data={data} height={800}>
-                    <EditingState onCommitChanges={this.onCommitChanges} />
-                    <EditRecurrenceMenu />
-                    <DayView startDayHour={6} endDayHour={24}/>
-                    <Appointments />
-                    <AppointmentTooltip
-                        showOpenButton
-                        showDeleteButton
-                    />
-                    <DragDropProvider />
-                    <AppointmentForm />
-                </Scheduler>
-            </Paper>
+            <>
+                <Paper>
+                    <Scheduler data={data} height={800}>
+                        <EditingState onCommitChanges={this.commitChanges} />
+                        <EditRecurrenceMenu />
+                        <DayView startDayHour={6} endDayHour={24}/>
+                        <Appointments />
+                        <AppointmentTooltip
+                            showOpenButton
+                            showDeleteButton
+                        />
+                        <DragDropProvider />
+                        <AppointmentForm />
+                    </Scheduler>
+                </Paper>
+                <button onClick={() => this.exportToJson(this.state.data)}>
+                    Export
+                </button>
+                <label htmlFor="avatar">Import:</label>
+                <input type="file" id="avatar" name="import" accept=".json" onChange={this.importJSON}/>
+            </>
         );
     }
 }
