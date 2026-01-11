cb44c70e3336cd1d23ad80fd0e4efee3932a3f24
Mert Serezli <mertserezli@gmail.com>
2026-01-11 15:18:59 +0300

    disable lint prop types

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,17 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+      "react/prop-types": "off", // disable prop-types rule
+    },
+  },
 ]);
